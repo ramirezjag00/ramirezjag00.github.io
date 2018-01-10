10daa2d83f84a9971c778354d6a3f7011f0a5c5c
ramirezjag00 <33412680+ramirezjag00@users.noreply.github.com>
2018-01-10 21:13:12 +0800

    v10_revised
    tweaked index page classes/ids for html/css, oocss
    projects page is mostly using flexbox and is more responsive

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById("myBtn").style.display = "block";
+		document.getElementById("btn-to-top").style.display = "block";
 	} else {
-		document.getElementById("myBtn").style.display = "none";
+		document.getElementById("btn-to-top").style.display = "none";
 	}
 }
 
